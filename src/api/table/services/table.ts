f3c465b117016a876514a8cffa3949b6fb5abe44
DungPope <58886762+DungPope@users.noreply.github.com>
2024-12-12 16:11:38 +0100

    Added table verification service

--- src/api/table/services/table.ts
+++ src/api/table/services/table.ts
@@ -4,4 +4,20 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::table.table');
+export default factories.createCoreService('api::table.table', ( ({strapi}) => ({
+
+    //Verify that exist a table with AccessCode <code>
+    //Return Table number if found or null if not 
+    async verify(code){
+        
+        const table = await strapi.db.query('api::table.table').findOne({
+            where: {
+                AccessCode: code
+            }
+        });
+
+        return table ? table.id : null;
+    }
+
+})
+));
