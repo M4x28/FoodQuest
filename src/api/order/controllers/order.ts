6ba1faab157bd983045ada0570c803a5ab96ddd4
Super3nry23 <enricobiagio@gmail.com>
2024-12-19 15:58:48 +0100

    ConfirmButton
    BuggedVers

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -1,7 +1,50 @@
 /**
- * order controller
+ * Controller per confermare l'ordine
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::order.order');
+export default factories.createCoreController('api::order.order', ({ strapi }) => ({
+
+    async confirmOrder(ctx) {
+        const { tableID, users_permissions_user } = ctx.request.body.data;
+
+        // Verifica che il tavolo sia specificato
+        if (!tableID) {
+            return ctx.badRequest('ID del tavolo mancante');
+        }
+
+        try {
+            // Trova l'ordine associato al tavolo e, se presente, all'utente
+            const filters: any = {
+                table: { documentId: tableID },
+                State: 'New'
+            };
+
+            if (users_permissions_user) {
+                filters.users_permissions_user = { documentId: users_permissions_user };
+            }
+
+            const order = await strapi.documents('api::order.order').findMany({
+                filters,
+                limit: 1
+            });
+
+            if (order.length === 0) {
+                return ctx.notFound('Ordine non trovato');
+            }
+
+            // Aggiorna lo stato dell'ordine a 'Pending'
+            const updatedOrder = await strapi.documents('api::order.order').update({
+                documentId: order[0].documentId,
+                data: { State: 'Pending' },
+            });
+
+            return ctx.send({ message: 'Ordine confermato con successo', data: updatedOrder });
+        } catch (error) {
+            strapi.log.error('Errore durante la conferma dell ordine: ', error);
+            return ctx.internalServerError('Errore del server');
+        }
+    },
+
+}));
